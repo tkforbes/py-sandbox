d0c56511bfbd7b0ad601abc522ee32c98dd58b3a
Tim Forbes <timothy.forbes@canada.ca>
2020-09-29 10:41:25 -0400

    Load the OGN registrations just once. Much more efficient than loading them on instantiation of object.

--- ognRegistrations.py
+++ ognRegistrations.py
@@ -1,25 +1,23 @@
 #
-# TODO : currently loads from file every time instaniated. Fix.
 
-class OgnRegistration:
-    def __init__(self):
-        self.OgnDict = {
+OgnDict = {
+# sample of format...
 #                 '000368': 'C-GORE',
 #                 '003974': 'C-GFOP',
 #                 'FF4A68': 'C-GINY'
         }
-        with open('aircraft.canada', 'r') as ognRegistrations:
-            for detail in ognRegistrations:
-                #.split(",")
-                values = detail.strip().split(",")
 
-                radioId = values[1].strip("\'")
-                aircraftId = values[3].strip("\'")
+with open('aircraft.canada', 'r') as ognRegistrations:
+    for detail in ognRegistrations:
+        #.split(",")
+        values = detail.strip().split(",")
 
-                # append
-                self.OgnDict[radioId] = aircraftId
-                #print(detail[1], detail[3])
-        #print(self.OgnDict)
+        radioId = values[1].strip("\'")
+        aircraftId = values[3].strip("\'")
 
+        # append
+        OgnDict[radioId] = aircraftId
+
+class OgnRegistration:
     def getAircraft(self, radioId):
-        return self.OgnDict.get(radioId, "not found")
+        return OgnDict.get(radioId, "not found")
