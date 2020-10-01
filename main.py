0bff20dca718e1256dcf7295bfba84fee40c7285
Tim Forbes <timothy.forbes@canada.ca>
2020-10-01 12:34:44 -0400

    clean up.

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-
-import io
-
 import pynmea2
-
-import math
-
-import sys
 
 from airfield import Airfield
 from flarmProximateAircraft import FlarmProximateAircraft
 from ognRegistrations import OgnRegistration
 from flarmPriorityIntruder import FlarmPriorityIntruder
 
+r = OgnRegistration ()
+print(r.getAircraft('r'))
+
 airfield = Airfield(81, 45.062101, 075.374431)
 proximateAircraft = FlarmProximateAircraft()
 priorityIntruder = FlarmPriorityIntruder()
 
-def is_integer(n):
-    try:
-        int(n)
-        return True
-    except ValueError:
-        return False
+
+#answer = FlarmPriorityIntruder.sixteenWindCompassPoint(1)
+#print("bearing: ", bearing, "ans:", answer)
+
+# def is_integer(n):
+#     try:
+#         int(n)
+#         return True
+#     except ValueError:
+#         return False
 
 nmea = open('data.nmea', 'r')
 
@@ -36,9 +36,9 @@ for line in nmea:
     except pynmea2.ParseError:
         # ignore sentences that can't be parsed
         continue
-    except:
-        # ignore sentences that raise any other error
-        continue
+    # except:
+    #     # ignore sentences that raise any other error
+    #     continue
 
     #print(repr(msg))
 
@@ -46,23 +46,23 @@ for line in nmea:
 
     # don't do anything with Flarm sentences until the airfield
     # has a valid datestamp.
-    if (type(msg) is pynmea2.nmea.ProprietarySentence and
-        airfield.validDatestamp()):
-        if (msg.manufacturer == "FLA"):
+    if (isinstance(msg, pynmea2.nmea.ProprietarySentence) and
+            airfield.validDatestamp()):
+        if msg.manufacturer == "FLA":
             # this is a Flarm sentence. try to set it.
-            if (proximateAircraft.set(airfield.timestamp, msg)):
-                proximateAircraft.print()
-            elif (priorityIntruder.set(airfield.timestamp, msg)):
-                priorityIntruder.print(airfield)
+            if proximateAircraft.set(airfield.timestamp, msg):
+                proximateAircraft.printt()
+            elif priorityIntruder.set(airfield.timestamp, msg):
+                priorityIntruder.printt(airfield)
 
-    elif (msg.sentence_type == 'RMC'):
+    elif msg.sentence_type == 'RMC':
         # this sentence contains the current date
 
         # update the date in the airfield. the date is very important!
         airfield.setDatestamp(msg.datestamp)
         airfield.setCourseTrue(msg.true_course)
         #print("course true:", msg.true_course)
-    elif (msg.sentence_type == 'GGA' and airfield.validDatestamp()):
+    elif msg.sentence_type == 'GGA' and airfield.validDatestamp():
         # this sentence has the airfield timestamp, lat, lon, elevation
         airfield.set(msg)
 
