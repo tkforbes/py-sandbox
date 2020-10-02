1b6c769873e7a3bc057b34adcced99d629942609
tkforbes <tfgreenbullfrog.com>
2020-10-02 11:11:44 -0400

    remove some print statements.

--- aircraft.py
+++ aircraft.py
@@ -8,10 +8,7 @@ class Aircraft:
         self.sentences = []
 
     def append(self, sentence):
-        print("appending :", sentence)
-        print("list len before:", len(self.sentences))
         self.sentences.append(sentence)
-        print("list len after :", len(self.sentences))
 
     def getSentences(self):
         return self.sentences
