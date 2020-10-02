a22a449d4eca590f8e97c9bff95e397efc56a318
tkforbes <tfgreenbullfrog.com>
2020-10-02 18:05:51 -0400

    improving validation in Airfield class.

--- airfield.py
+++ airfield.py
@@ -47,6 +47,135 @@ class Airfield:
     def set(self, nmea):
         if (nmea.sentence_type == 'GGA'):
             gga = nmea
+
+            print(repr(gga))
+            ## UTC time
+            try:
+                utcTime = gga.timestamp
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # position fix indicator
+            try:
+                gpsQual = int(gga.gps_qual)
+                if not (0 <= gpsQual <= 6):
+                    raise Exception("position fix indicator must be between 1 and 6")
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            if not (gpsQual in [1, 2]):
+                # the fix method must be something useful to us
+                return False
+
+            # latitude
+            try:
+                lat = gga.lat
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # N/S indicator
+            try:
+                latDir = gga.lat_dir
+                latDir = 'N'
+                if not (latDir in ['N', 'S']):
+                    raise Exception("lat dir must be N or S.")
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # longitude
+            try:
+                lon = gga.lon
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # E/W indicator
+            try:
+                lonDir = gga.lon_dir
+                lonDir = 'E'
+                if not (lonDir in ['E', 'W']):
+                    raise Exception("lon dir must be E or W.")
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # position fix indicator
+            try:
+                gpsQual = int(gga.gps_qual)
+                if not (0 <= gpsQual <= 6):
+                    raise Exception("position fix indicator must be between 1 and 6")
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # satellites used
+            try:
+                numSats = int(gga.num_sats)
+                if not (0 <= numSats <= 24 ):
+                    raise Exception("number of satellites must be between 0 and 24")
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+
+            # hdop
+            try:
+                horizontalDil = gga.horizontal_dil
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # MSL altitude
+            try:
+                altitude = gga.altitude
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+
+            # MSL altitude units
+            try:
+                altitudeUnits = gga.altitude_units
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+
+
+            # geoid separation
+            try:
+                geoSep = gga.geo_sep
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # geoid separation units
+            try:
+                geoSepUnits = gga.geo_sep_units
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # age of diff. corr.
+            try:
+                ageGpsData = gga.age_gps_data
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
+            # diff. ref. station ID
+            try:
+                refStationId = int(gga.ref_station_id)
+                if not (0 <= refStationId <= 4095):
+                    raise Exception("reference station id must be between 0 and 4095")
+            except Exception as e:
+                print(gga, ":", e)
+                sys.exit()
+
             if (Airfield.isvalid(gga)):
                 #print(repr(nmea))
                 if (gga.altitude > self.ignoreAbove or
