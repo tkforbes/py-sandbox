30b2cc27593dcaf3a6f8cb3fed560eec38dffb8e
tkforbes <tfgreenbullfrog.com>
2020-10-28 15:00:41 -0400

    linting...

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import datetime
 import pynmea2
 #import geopy
 #import geopy.distance
@@ -10,19 +11,14 @@ from event import Event
 from event import TakeoffEvent
 #from event import LandingEvent
 from event import LaunchEvent
-import datetime
 from aircraft import Aircraft
 
-
-observation = Observation()
 
 def eachAircraft():
 
     groundstation = Groundstation()
 
-
-    aircraftSeen = {
-    }
+    aircraft_seen = {}
 
     nmea = open('data.nmea', 'r')
 
@@ -36,107 +32,105 @@ def eachAircraft():
         except pynmea2.ParseError:
             # ignore sentences that can't be parsed
             continue
-        except Exception:
-            continue
 
         # ignore Flarm PFLAU sentences
-        if (Observation.isPflauSentence(sentence)):
+        if Observation.isPflauSentence(sentence):
             continue
 
         # The groundstation must have received the UTC time from the GPS
         # before we permit any processing of Flarm PFLAA observations.
         if (groundstation.validTime() and
-            Observation.isPflaaSentence(sentence)):
+                Observation.isPflaaSentence(sentence)):
             observation = Observation()
             if observation.set(groundstation, sentence):
-                aircraftId = observation.getAircraftId()
-                if not (aircraftId in aircraftSeen):
-                    aircraftSeen[aircraftId] = Aircraft(aircraftId)
-                aircraftSeen[aircraftId].appendObservations(observation)
+                aircraft_id = observation.getAircraftId()
+                if aircraft_id not in aircraft_seen:
+                    aircraft_seen[aircraft_id] = Aircraft(aircraft_id)
+                aircraft_seen[aircraft_id].appendObservations(observation)
 
         elif sentence.sentence_type == 'RMC':
             # this sentence contains the current date
             groundstation.setDate(sentence.datestamp)
             groundstation.set(sentence)
         elif (sentence.sentence_type == 'GGA'
-                and groundstation.validDate()
-                and commas == 14):
+              and groundstation.validDate()
+              and commas == 14):
 
             # this sentence has the groundstation timestamp, lat, lon, elevation
             groundstation.set(sentence)
 
-    print("%s" % list(aircraftSeen.keys()))
+    print("%s" % list(aircraft_seen.keys()))
 
     groundstation.report()
 
     # reg = "C-GFOP"
     # print("")
     # print(reg)
-    # aircraftSeen[reg].printObservations()
+    # aircraft_seen[reg].printObservations()
 
     print("")
     print("FLIGHTS PER AIRCRAFT")
     print("====================")
-    for ac in list(aircraftSeen.keys()):
+    for aircraft in list(aircraft_seen.keys()):
         print("")
-        print(ac)
-        aircraftSeen[ac].detectEvents()
-        aircraftSeen[ac].reportEvents()
+        print(aircraft)
+        aircraft_seen[aircraft].detectEvents()
+        aircraft_seen[aircraft].reportEvents()
 
-    flightSheet = []
-    for ac in list(aircraftSeen.keys()):
-        reg = aircraftSeen[ac].getAircraftId()
-        theEvents = aircraftSeen[ac].events
-        for e in theEvents:
-            if type(e) is TakeoffEvent:
-                le = LaunchEvent(reg, e.getTimestamp(), e.getLat(), e.getLon(),
-                                 e.getAltitudeAGL(), e.getTrack(), e.speed)
-                flightSheet.append(le)
+    flight_sheet = []
+    for aircraft in list(aircraft_seen.keys()):
+        reg = aircraft_seen[aircraft].getAircraftId()
+        the_events = aircraft_seen[aircraft].events
+        for event in the_events:
+            if isinstance(event, TakeoffEvent):
+                launch_event = LaunchEvent(reg, event.getTimestamp(), event.getLat(), event.getLon(),
+                                           event.getAltitudeAGL(), event.getTrack(), event.speed)
+                flight_sheet.append(launch_event)
 
-    flightSheet.sort()
+    flight_sheet.sort()
 
 
     print("")
     print("Flight Sheet")
     print("============")
 
-    y = 1
+    item_num = 1
 
     # we work with pairs, so starting at the second takeoff and looking at
     # the first...
-    for n in range(1, len(flightSheet)):
-        previous = flightSheet[n-1].getTimestamp()
-        current = flightSheet[n].getTimestamp()
-        if n < len(flightSheet)-1:
-            next = flightSheet[n+1].getTimestamp()
+    for ndx in range(1, len(flight_sheet)):
+        previous = flight_sheet[ndx-1].getTimestamp()
+        current = flight_sheet[ndx].getTimestamp()
+        if ndx < len(flight_sheet)-1:
+            nxt = flight_sheet[ndx+1].getTimestamp()
         else:
-            next = Event.event_not_detected
+            nxt = Event.event_not_detected
 
         # are the current and previous takeoff within 30 seconds?
         if (previous - datetime.timedelta(seconds=30) <
                 current <
                 previous + datetime.timedelta(seconds=30)):
             # this is a takeoff pair
-            print("%02d" % y,
+            print("%02d" % item_num,
                   "Launch ",
                   current,
-                  "R%02d" % flightSheet[n].getRwy(),
-                  flightSheet[n].getReg(),
-                  flightSheet[n-1].getReg()
+                  "R%02d" % flight_sheet[ndx].getRwy(),
+                  flight_sheet[ndx].getReg(),
+                  flight_sheet[ndx-1].getReg()
                   )
-            y+=1
+            item_num += 1
         # the pair is not a match. what about the upcoming pair?
-        elif next - datetime.timedelta(seconds=30) < current < next + datetime.timedelta(seconds=30):
+        elif nxt - datetime.timedelta(seconds=30) < current < nxt + datetime.timedelta(seconds=30):
             # do nothing. the next iteration of the loop will process the pair
             pass
         else:
             # this is a lone takeoff event
-            print("%02d" % y,
+            print("%02d" % item_num,
                   "Takeoff", current,
-                  "R%02d" % flightSheet[n].getRwy(),
-                  flightSheet[n].getReg()
+                  "R%02d" % flight_sheet[ndx].getRwy(),
+                  flight_sheet[ndx].getReg()
                   )
-            y+=1
+            item_num += 1
 
     return
 
